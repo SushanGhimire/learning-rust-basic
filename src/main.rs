2d89da178f48a97aeeb4dee7493be731b87a8cff
SushanGhimire <sushangmi55@gmail.com>
2023-02-02 21:12:50 +0545

    [Variables and Mutability]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    const LET_X_VAR:u32= 60*10*20;
+    println!("Value of x is: {x}");
+    x= LET_X_VAR;
+    println!("Value of x is: {x}");
+
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+    println!("The value of y is: {y}");
+
 }
