2c29a1b0038f73ddd9110c115e874ff17987e5e2
SushanGhimire <sushangmi55@gmail.com>
2023-02-02 21:40:02 +0545

    [Data Types]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 fn main() {
-    let mut x = 5;
-    const LET_X_VAR:u32= 60*10*20;
-    println!("Value of x is: {x}");
-    x= LET_X_VAR;
-    println!("Value of x is: {x}");
+    //interger
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("{guess}");
 
-    let y = 5;
-    let y = y + 1;
-    {
-        let y = y * 2;
-        println!("The value of y in the inner scope is: {y}");
-    }
-    println!("The value of y is: {y}");
+    //float
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    println!("{x}-{y}");
 
+    //boolean
+    let t: bool = true;
+    println!("{t}");
+
+    //string
+    let new_string = "Hello world";
+    println!("{new_string}");
+
+    //character
+    let new_char: char = 'Z';
+    println!("{new_char}");
+
+    //tuple
+    let tup: (i32, f64, u8) = (300, 1.0, 1);
+    let (x, _y, _z): (i32, f64, u8) = tup;
+    println!("{x}");
+    let tup_x = tup.0;
+    println!("{tup_x}");
+
+    //array
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+    println!("{first}");
+    println!("{second}")
 }
